dd5d87cdd6eab7b187896c9b0650925f793dec7e
Panitnun Suvannabun <Panitnun.6243@gmail.com>
2021-11-25 02:30:05 +0700

    Add form event

--- src/components/formComponent.js
+++ src/components/formComponent.js
@@ -1,15 +1,26 @@
 
 const FormComponent=()=>{
+const inputTitle=(event)=>{
+    console.log( event.target.value)
+   
+}
+const inputAmount=(event)=>{
+    console.log(event.target.value)
+}
+const saveItem=(event)=>{
+    event.preventDefault()//ไม่ให้จอเรนเดอใหม่ ดักจับข้อมูลให้เเสดงผลค้างไว้ 
+    console.log('Already save')
+}
     return(
         <div className="form-container">
-            <form>
+            <form onSubmit={saveItem}>
                 <div className="list-item">
                     <label>List :</label>
-                    <input type="text" placeholder="Your income or expense"/>
+                    <input type="text" placeholder="Your income or expense" onChange={inputTitle}/>
                 </div>
                 <div className="amount-item">
                     <label>Amount :</label>
-                    <input type="text" placeholder="Costs"/>
+                    <input type="text" placeholder="Costs" onChange={inputAmount}/>
                 </div>
                 <div className="button-item">
                     <button type="submit" >&#128176; Add to your list</button>
