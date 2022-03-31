494c62c92a589d2d5f102f3337617b6b14360678
Panitnun Suvannabun <Panitnun.6243@gmail.com>
2022-03-31 19:14:37 +0700

    Add Item Style

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,17 +1,24 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
-const Item = (props) =>{
-    const {title, amount} = props;
-    return (
-    <li className="item">
-      {title} <span>{amount}</span>
+const Item = (props) => {
+  const { title, amount } = props;
+  const status = amount > 0 ? "income" : "expense";
+  const symbol = amount > 0 ? "+" : "-";
+  return (
+    // ค่าตัวเลขติดลบจะเข้าเอาเครื่องหมายลบเข้ามาด้วย ดังนั้นต้องเคลียค่าโดยใช้ absolute
+    <li className={status}>
+      {title}{" "}
+      <span>
+        {symbol}
+        {Math.abs(amount)}
+      </span>
     </li>
   );
-} 
+};
 
-Item.propTypes={
-  title:PropTypes.string.isRequired, 
-  amount:PropTypes.number.isRequired
-}
+Item.propTypes = {
+  title: PropTypes.string.isRequired,
+  amount: PropTypes.number.isRequired,
+};
 
 export default Item;
